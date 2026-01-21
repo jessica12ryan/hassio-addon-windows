9062798b8a7b278fb0f433f6fea2105793d0910f
jessica12ryan <jessica12ryan@gmail.com>
2026-01-21 18:55:15 -0400

    Update run.sh

--- addon-windows/run.sh
+++ addon-windows/run.sh
@@ -2,16 +2,19 @@
 set -e
 
 # Read HA options
+KVM=$(jq -r '.KVM' /data/options.json)
 DISK_SIZE=$(jq -r '.DISK_SIZE' /data/options.json)
 VERSION=$(jq -r '.VERSION' /data/options.json)
 LANGUAGE=$(jq -r '.LANGUAGE' /data/options.json)
 
 # Export as environment variables for dockur/windows
+export KVM
 export DISK_SIZE
 export VERSION
 export LANGUAGE
 
 echo "Starting Windows VM"
+echo "KVM=$KVM"
 echo "DISK_SIZE=$DISK_SIZE"
 echo "VERSION=$VERSION"
 echo "LANGUAGE=$LANGUAGE"
