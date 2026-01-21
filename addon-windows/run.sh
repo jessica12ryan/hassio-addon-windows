1daaba7bc55a787ae41d13828cebeb9cdd972456
jessica12ryan <jessica12ryan@gmail.com>
2026-01-21 18:52:33 -0400

    Create run.sh

---
+++ addon-windows/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bash
+set -e
+
+# Read HA options
+DISK_SIZE=$(jq -r '.DISK_SIZE' /data/options.json)
+VERSION=$(jq -r '.VERSION' /data/options.json)
+LANGUAGE=$(jq -r '.LANGUAGE' /data/options.json)
+
+# Export as environment variables for dockur/windows
+export DISK_SIZE
+export VERSION
+export LANGUAGE
+
+echo "Starting Windows VM"
+echo "DISK_SIZE=$DISK_SIZE"
+echo "VERSION=$VERSION"
+echo "LANGUAGE=$LANGUAGE"
+
+# Start the original container entrypoint
+exec /entrypoint.sh
