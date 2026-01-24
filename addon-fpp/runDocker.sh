e638afac8414d588d1ae098b99a9c47a2b97cef3
jessica12ryan <jessica12ryan@gmail.com>
2026-01-24 19:29:05 -0400

    Add files via upload

---
+++ addon-fpp/runDocker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ ! -f /opt/fpp/src/fppinit ]; then
+    cd /opt/fpp/src
+    CPUS=$(/opt/fpp/scripts/functions ComputeMakeParallelism)
+    make -j ${CPUS}
+fi
+
+/opt/fpp/src/fppinit start
+/opt/fpp/scripts/fppd_start
+
+mkdir /run/php
+/usr/sbin/php-fpm8.2 --fpm-config /etc/php/8.2/fpm/php-fpm.conf
+/usr/sbin/apache2ctl -D FOREGROUND
